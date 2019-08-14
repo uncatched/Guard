d0439be780f4b0b5096bb7e5414fe147d6ee1c89
Denys Litvinskyi <litvinskyi.dev@gmail.com>
2019-08-14 19:09:44 +0300

    Fix empty view on storage

--- Guard/Modules/Storage/StorageTableViewController.swift
+++ Guard/Modules/Storage/StorageTableViewController.swift
@@ -11,17 +11,27 @@ import UIKit
 final class StorageTableViewController: UITableViewController {
     
     // MARK: - Properties
-    private var data: [StorageData]!
+    private var data: [StorageData]! {
+        didSet {
+            emptyView.isHidden = !data.isEmpty
+        }
+    }
     private var emptyLabel: UILabel!
+    @IBOutlet private weak var emptyView: UIView!
     
     // MARK: - Life Cycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     
         data = StorageManager.data
-        tableView.isScrollEnabled = !data.isEmpty
-        tableView.separatorStyle = !data.isEmpty ? .singleLine : .none
+        
         tableView.reloadData()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        emptyView.frame = tableView.bounds
     }
 }
 
@@ -58,42 +68,19 @@ extension StorageTableViewController {
 // MARK: - UITableViewDelegate/DataSource
 extension StorageTableViewController {
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count == 0 ? 1 : data.count
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if data.count == 0 {
-            let navigationBarHeight: CGFloat = navigationController?.navigationBar.bounds.height ?? 0.0
-            let tabBarHeight: CGFloat = tabBarController?.tabBar.bounds.height ?? 0.0
-            return tableView.bounds.size.height - (navigationBarHeight + tabBarHeight + 35)
-        } else {
-            return 50
-        }
+        return data.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if data.count == 0 {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "EmptyStorageCell") else {
-                return UITableViewCell()
-            }
-            cell.selectionStyle = .none
-            
-            return cell
-        } else {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "StorageCell") else {
-                return UITableViewCell()
-            }
-            
-            let item = data[indexPath.row]
-            cell.textLabel?.text = item.title ?? "Item"
-            
-            return cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "StorageCell") else {
+            return UITableViewCell()
         }
+        
+        let item = data[indexPath.row]
+        cell.textLabel?.text = item.title ?? "Item"
+        
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
@@ -112,10 +99,6 @@ extension StorageTableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard data.count > 0 else {
-            return
-        }
-        
         let item = data[indexPath.row]
         guard let detailsViewController = storyboard?.instantiateViewController(withIdentifier: "PasswordDetailsViewController") as? PasswordDetailsViewController else {
             return
