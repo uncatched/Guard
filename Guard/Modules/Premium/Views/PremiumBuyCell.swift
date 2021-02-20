91ad3ef64099100f82d8ed814582e7dd440939ee
Denys Litvinskyi <litvinskiidev@gmail.com>
2021-02-20 11:43:53 +0200

    Fixes

--- Guard/Modules/Premium/Views/PremiumBuyCell.swift
+++ Guard/Modules/Premium/Views/PremiumBuyCell.swift
@@ -13,7 +13,6 @@ final class PremiumBuyCell: UITableViewCell {
     // MARK: - Outlets
     @IBOutlet private(set) weak var buyButton: UIButton!
     @IBOutlet private weak var protectLabel: UILabel!
-    @IBOutlet private weak var trialLabel: UILabel!
     @IBOutlet private weak var monthLabel: UILabel!
     
     // MARK: - Life Cycle
@@ -29,7 +28,6 @@ extension PremiumBuyCell {
     
     private func setupSubviews() {
         protectLabel.text = NSLocalizedString("premium_label_protectYourPhone", comment: "")
-        trialLabel.text = NSLocalizedString("premium_label_startFreeTrial", comment: "")
         monthLabel.text = NSLocalizedString("premium_label_thenPerMonth", comment: "")
         buyButton.setTitle(NSLocalizedString("premium_button_tryFree", comment: ""), for: .normal)
         buyButton.layer.cornerRadius = 4.0
